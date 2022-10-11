bed12ca8d3b1b7f54e2ec7f1b544c1eb94cfdae4
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-10-10 22:49:39 -0300

    .

--- src/aula1.dart
+++ src/aula1.dart
@@ -1,3 +1,29 @@
 main(){
   print("Ola mundo");
+  int ano = 2021;
+  print("Ola pessoa ${ano} ano legal");
+  if(ano % 2 == 0){
+    print("O numero é par");
+  }else{
+    print("Numero é impar");
+  }
+
+
+  var lista = [1,2,3];
+  lista.add(5);
+
+  print(lista);
+
+  Map <String, List> alunos = {
+  "Joao": [10, 9, 8]
+  };
+ print(alunos["Joao"]);
+
+
+
+
+
+
+
+  
 }
